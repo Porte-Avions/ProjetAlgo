f52592fa4d87b7b03d921afe64b773d7df9abaf1
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-06-26 23:38:38 +0200

    start dev mutation

--- algoGen.py
+++ algoGen.py
@@ -1,6 +1,144 @@
 import time
+import numpy as np
+from random import randint, randrange
+import random
+from numpy.random import choice
+import copy
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+import os
 
 start = time.time() #Début du timer
+
+def voisinsSommetGrapheMatrice(matrice, sommet):
+    liste = matrice[sommet]
+    voisins = \
+        [i for i, value in enumerate(liste) if liste[i] != 0] #SOLUTION
+    return voisins
+
+def field(matrice, i, j):
+    inf = 1
+    sup = 50
+    _cost_essence = 1.80
+    
+    field = []
+    km=(70, 80, 90, 110, 130)
+    dist = randint(inf, sup)
+    velocity = random.choice(km)
+    time = round((dist/velocity)*60)
+    conso = 8
+    peage_cost = 0
+    if velocity == 130:
+        conso += conso*0.2
+        peage_cost += round((1.9+4.5*(dist/sup)),1)
+    elif velocity == 110:
+        conso += conso*0.15
+    price_essence = round((((dist*conso)/100) * _cost_essence),1)
+    total_price = round((peage_cost + price_essence),1)
+    field.append(1)
+    field.append(dist)
+    field.append(velocity) #supprimer
+    field.append(time)
+    field.append(conso)
+    field.append(peage_cost)
+    field.append(price_essence)
+    field.append(total_price)
+    matrice[i][j] = field
+    matrice[j][i] = field
+    
+def affiche_matrice(matrice):
+    for line in matrice:
+        print(line)
+        
+def generate_matrice(M,N):
+    matrice = []
+    for i in range(M):
+        line=[]
+        for j in range(N):
+            line.append(0)
+        matrice.append(line)
+    return matrice
+        
+def complete_matrice(matrice, M):
+    for i in range(M):
+        _nb_element = 2
+        _element = random.sample(range(M), _nb_element)
+        try:
+            for j in _element:
+                if i != j :
+                    field(matrice, i, j)
+        except:
+            _element = []
+            _element = random.sample(range(M), _nb_element)
+        finally:
+            for j in _element:
+                if i != j :
+                    field(matrice, i, j)
+        _element = []
+    return matrice
+
+def organize_matrice(matrice):
+    for i in range(0, len(matrice)):
+        for j in range(i+1, len(matrice)):
+            matrice[j][i] = matrice [i][j]                      
+            
+def info_matrice(matrice):
+    _map = {}
+    _list = []
+
+    dico = {
+    "bool_chemin": None,
+    "voisin": None,
+    "distance": None, 
+    "vitesse": None, # supprimer
+    "temps": None,
+    "consommation": None,
+    "peage": None,
+    "cout": None,
+    "total": None
+    }
+    
+    for i in range(0, len(matrice)):
+        _list.clear()
+        for j in range(0, len(matrice)):
+            if matrice[i][j] != 0:
+                dico["bool_chemin"] = matrice[i][j][0]
+                dico["voisin"] = j
+                dico["distance"] = matrice[i][j][1]
+                dico["vitesse"] = matrice[i][j][2]
+                dico["temps"] = matrice[i][j][3]
+                dico["consommation"] = matrice[i][j][4]
+                dico["peage"] = matrice[i][j][5]
+                dico["cout"] = matrice[i][j][6]
+                dico["total"] = matrice[i][j][7]
+                _list.append(copy.deepcopy(dico))    
+        _map[i] = copy.deepcopy(_list)
+    return _map
+
+def mutation(chromosone, probMutation, nbVille):
+    if probMutation > random.randint(0,100):
+        firstGene = random.randint(0, nbVille)
+        secondGene = random.randint(0, nbVille)
+        chromosone[]
+
+
+def fitness(chromosone):
+    distance = 0
+    for i in range(0,len(chromosone)):
+        distance += chromosone[i]["distance"]
+    return distance
+
+
+
+
+matrice = generate_matrice(10, 10)
+complete_matrice(matrice, 10)
+organize_matrice(matrice)
+affiche_matrice(matrice)
+dico = info_matrice(matrice)
+
+print(dico)
 
 
 
